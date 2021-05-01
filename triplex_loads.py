dabd07d2aa2ec62548334901d6e5e513f3cc8962
MidrarAdham <midrar@pdx.edu>
2021-04-30 22:40:28 -0700

    Add source code and triplex_loads script

---
+++ triplex_loads.py
@@ -0,0 +1,28 @@
+from functions import *
+
+
+
+i = 0
+print("Associated Node:")
+
+node = input()
+print("enter phases:")
+
+phases = input()
+print("enter a csv file name:")
+
+file_name = input()
+
+house_player = "_player"
+
+print("how many instances?")
+
+counter = input()
+
+while i < int(counter):
+
+	trip_player = trip_load_with_player("House_"+node+"_"+str(i)+"_"+phases,phases,house_player+str(i),file_name+str(i))
+	print(trip_player)
+	i = i + 1
+
+
